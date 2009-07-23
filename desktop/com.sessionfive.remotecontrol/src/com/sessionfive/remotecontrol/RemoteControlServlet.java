cf3222c021b43bc366dde7c892426d114b0da568
Matthias Luebken <matthias.luebken@gmail.com>
2009-07-23 21:43:27 +0200

    debug infos

--- desktop/com.sessionfive.remotecontrol/src/com/sessionfive/remotecontrol/RemoteControlServlet.java
+++ desktop/com.sessionfive.remotecontrol/src/com/sessionfive/remotecontrol/RemoteControlServlet.java
@@ -3,6 +3,8 @@ package com.sessionfive.remotecontrol;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -20,12 +22,23 @@ public class RemoteControlServlet extends HttpServlet {
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		System.out.println("servlet initialized");
+
+		// determine debug info
+		String port = System.getProperty("org.eclipse.equinox.http.jetty.http.port");
+		String host = "";
+		try {
+			InetAddress addr = InetAddress.getLocalHost();
+			host = addr.getHostAddress();
+		} catch (UnknownHostException e) {
+		}
+
+		System.out.println("servlet initialized: " + host + ":" + port);
+
 	}
 
 	@Override
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String pathInfo = request.getPathInfo();
 
 		if ("/forward".equals(pathInfo)) {
@@ -45,14 +58,15 @@ public class RemoteControlServlet extends HttpServlet {
 		}
 	}
 
-	private void returnKeyFrameAt(HttpServletRequest request,
-			HttpServletResponse response) throws IOException {
+	private void returnKeyFrameAt(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
 		String keyFrameNo = request.getParameter("at");
 		if (keyFrameNo != null) {
 			int parsedNumber = Integer.parseInt(keyFrameNo);
-			if (parsedNumber < SessionFiveApplication.getInstance().getAnimationController().getNumberOfKeyFrames()) {
+			if (parsedNumber < SessionFiveApplication.getInstance().getAnimationController()
+					.getNumberOfKeyFrames()) {
 				byte[] image = SessionFiveApplication.getInstance().getKeyFrame(parsedNumber);
-				
+
 				if (image != null) {
 					response.setContentType("image/jpg");
 					response.setStatus(HttpServletResponse.SC_OK);
@@ -60,7 +74,7 @@ public class RemoteControlServlet extends HttpServlet {
 					ServletOutputStream out = response.getOutputStream();
 					out.write(image);
 					out.close();
-				} 
+				}
 			}
 		}
 	}
@@ -69,15 +83,17 @@ public class RemoteControlServlet extends HttpServlet {
 		String toParameter = request.getParameter("to");
 		if (toParameter != null) {
 			int parsedNumber = Integer.parseInt(toParameter);
-			if (parsedNumber < SessionFiveApplication.getInstance().getAnimationController().getNumberOfKeyFrames()) {
+			if (parsedNumber < SessionFiveApplication.getInstance().getAnimationController()
+					.getNumberOfKeyFrames()) {
 				SessionFiveApplication.getInstance().getAnimationController().goTo(parsedNumber);
 			}
 		}
 	}
 
-	private void returnNumberOfKeyFrames(HttpServletRequest request,
-			HttpServletResponse response) throws IOException {
-		int numberOfKeyFrames = SessionFiveApplication.getInstance().getAnimationController().getNumberOfKeyFrames();
+	private void returnNumberOfKeyFrames(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		int numberOfKeyFrames = SessionFiveApplication.getInstance().getAnimationController()
+				.getNumberOfKeyFrames();
 
 		response.setContentType("text/plain");
 		response.setStatus(HttpServletResponse.SC_OK);
@@ -88,20 +104,20 @@ public class RemoteControlServlet extends HttpServlet {
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		super.doGet(request, response);
 	}
 
-	private void returnImages(HttpServletRequest request,
-			HttpServletResponse response, String pathInfo) throws IOException {
+	private void returnImages(HttpServletRequest request, HttpServletResponse response,
+			String pathInfo) throws IOException {
 		InputStream in;
 		if ("/nextImage".equals(pathInfo)) {
 			in = SessionFiveApplication.getInstance().getAnimationController().nextImage();
 		} else {// if ("/prevImage".equals(pathInfo)) {
 			in = SessionFiveApplication.getInstance().getAnimationController().prevImage();
 		}
-		
+
 		if (in != null) {
 			response.setContentType("image/jpg");
 			response.setStatus(HttpServletResponse.SC_OK);
@@ -114,7 +130,7 @@ public class RemoteControlServlet extends HttpServlet {
 			}
 			in.close();
 			out.close();
-		} 
+		}
 	}
 
 }
