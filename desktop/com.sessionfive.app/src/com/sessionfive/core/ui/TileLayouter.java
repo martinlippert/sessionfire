237b9a30d874e7e5036c9493cd5d33d4639c1321
Matthias Luebken <matthias.luebken@gmail.com>
2010-01-06 15:51:47 +0100

    neues tiling layout

--- desktop/com.sessionfive.app/src/com/sessionfive/core/ui/TileLayouter.java
+++ desktop/com.sessionfive.app/src/com/sessionfive/core/ui/TileLayouter.java
@@ -18,49 +18,47 @@ public class TileLayouter implements Layouter {
 
 	@Override
 	public void layout(Presentation presentation) {
-		final Camera startCamera = new Camera(-80f, -40f,
-				120f, 20f, -40f, 0f, 0f, 1f, 0f);
+		final Camera startCamera = new Camera(-80f, -40f, 120f, 20f, -50f, 0f, 0f, 1f, 0f);
 		presentation.setStartCamera(startCamera);
-		
+
 		List<Shape> shapes = presentation.getShapes(LayerType.CAMERA_ANIMATED);
 		int size = shapes.size();
-		
+
 		double sqrt = Math.sqrt(size);
-		long countPerLine = Math.round(sqrt + 1);
-		
+		long countPerLine = Math.round(sqrt - 1);
 		float z = 0f;
-		
+
 		float space = presentation.getSpace() * 2f;
 		float x = -space;
 		float y = -space;
-		
+
 		int lineCounter = 0;
 
-		for (Shape shape: presentation.getShapes(LayerType.CAMERA_ANIMATED)) {
+		for (Shape shape : presentation.getShapes(LayerType.CAMERA_ANIMATED)) {
 			shape.setPosition(x, y, z);
-			
+
 			x += space;
 			z += 0.01f;
-			
+
 			if (lineCounter == countPerLine) {
 				lineCounter = 0;
 				y -= space;
 				x = -space;
-			}
-			else {
+			} else {
 				lineCounter++;
 			}
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		return getName();
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
-		if (!(obj instanceof TileLayouter)) return false;
+		if (!(obj instanceof TileLayouter))
+			return false;
 		return toString().equals(obj.toString());
 	}
 
