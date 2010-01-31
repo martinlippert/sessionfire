c5a46a72ffc701e37166eae570469db476857a24
Martin Lippert <lippert@acm.org>
2010-02-01 00:00:51 +0100

    added size handling to position/size view and internal refactorings

--- desktop/com.sessionfive.app/src/com/sessionfive/core/ui/PositionSizeView.java
+++ desktop/com.sessionfive.app/src/com/sessionfive/core/ui/PositionSizeView.java
@@ -2,7 +2,6 @@ package com.sessionfive.core.ui;
 
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
-import javax.swing.JTextField;
 import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.event.ChangeEvent;
@@ -19,9 +18,9 @@ public class PositionSizeView extends AbstractView {
 	private JSpinner xField;
 	private JSpinner yField;
 	private JSpinner zField;
-	private JTextField widthField;
-	private JTextField heightField;
-	private JTextField depthField;
+	private JSpinner widthField;
+	private JSpinner heightField;
+	private JSpinner depthField;
 
 	public PositionSizeView(SelectionService selectionService) {
 		super(selectionService);
@@ -32,9 +31,13 @@ public class PositionSizeView extends AbstractView {
 		Shape[] shapes = getSelectedShapes();
 
 		if (shapes.length > 0) {
-			xField.setValue((double)shapes[0].getX());
-			yField.setValue((double)shapes[0].getY());
-			zField.setValue((double)shapes[0].getZ());
+			xField.setValue((double) getX());
+			yField.setValue((double) getY());
+			zField.setValue((double) getZ());
+
+			widthField.setValue((double) getWidth());
+			heightField.setValue((double) getHeight());
+			depthField.setValue((double) getDepth());
 		}
 	}
 
@@ -46,40 +49,75 @@ public class PositionSizeView extends AbstractView {
 				100d, 1d);
 		final SpinnerModel positionModelZ = new SpinnerNumberModel(0d, -100d,
 				100d, 1d);
-		
+
+		final SpinnerModel positionModelWidth = new SpinnerNumberModel(0d,
+				-100d, 100d, 1d);
+		final SpinnerModel positionModelHeight = new SpinnerNumberModel(0d,
+				-100d, 100d, 1d);
+		final SpinnerModel positionModelDepth = new SpinnerNumberModel(0d,
+				-100d, 100d, 1d);
+
 		xField = new JSpinner(positionModelX);
 		yField = new JSpinner(positionModelY);
 		zField = new JSpinner(positionModelZ);
 
-		ChangeListener changeListenerX = new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				setPositionX(((Double) positionModelX.getValue()).floatValue());
-			}
-		};
-		ChangeListener changeListenerY = new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				setPositionY(((Double) positionModelY.getValue()).floatValue());
-			}
-		};
-		ChangeListener changeListenerZ = new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				setPositionZ(((Double) positionModelZ.getValue()).floatValue());
-			}
-		};
-		xField.addChangeListener(changeListenerX);
-		yField.addChangeListener(changeListenerY);
-		zField.addChangeListener(changeListenerZ);
+		widthField = new JSpinner(positionModelWidth);
+		heightField = new JSpinner(positionModelHeight);
+		depthField = new JSpinner(positionModelDepth);
 
-		widthField = HudWidgetFactory.createHudTextField("");
-		heightField = HudWidgetFactory.createHudTextField("");
-		depthField = HudWidgetFactory.createHudTextField("");
+		ChangeListener xChangeListener = new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				setPosition(((Double) positionModelX.getValue()).floatValue(),
+						getY(), getZ());
+			}
+		};
+		ChangeListener yChangeListener = new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				setPosition(getX(), ((Double) positionModelY.getValue())
+						.floatValue(), getZ());
+			}
+		};
+		ChangeListener zChangeListener = new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				setPosition(getX(), getY(),
+						((Double) positionModelZ.getValue()).floatValue());
+			}
+		};
+		xField.addChangeListener(xChangeListener);
+		yField.addChangeListener(yChangeListener);
+		zField.addChangeListener(zChangeListener);
+
+		ChangeListener widthChangeListener = new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				setSize(((Double) positionModelWidth.getValue()).floatValue(),
+						getHeight(), getDepth());
+			}
+		};
+		ChangeListener heightChangeListener = new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				setSize(getWidth(), ((Double) positionModelHeight.getValue())
+						.floatValue(), getDepth());
+			}
+		};
+		ChangeListener depthChangeListener = new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				setSize(getWidth(), getHeight(), ((Double) positionModelDepth
+						.getValue()).floatValue());
+			}
+		};
+		widthField.addChangeListener(widthChangeListener);
+		heightField.addChangeListener(heightChangeListener);
+		depthField.addChangeListener(depthChangeListener);
 
 		FormLayout layout = new FormLayout(
 				"fill:pref:grow, fill:pref:grow, fill:pref:grow",
-				"pref, 0dlu, pref, 0dlu, pref");
+				"pref, 0dlu, pref, 0dlu, pref, 0dlu, pref");
 
 		JPanel panel = new JPanel(layout);
 		panel.setOpaque(false);
@@ -92,46 +130,61 @@ public class PositionSizeView extends AbstractView {
 		panel.add(HudWidgetFactory.createHudLabel("Z"), cc.xy(3, 1));
 		panel.add(zField, cc.xy(3, 3));
 
+		panel.add(HudWidgetFactory.createHudLabel("Width"), cc.xy(1, 5));
+		panel.add(widthField, cc.xy(1, 7));
+		panel.add(HudWidgetFactory.createHudLabel("Height"), cc.xy(2, 5));
+		panel.add(heightField, cc.xy(2, 7));
+		panel.add(HudWidgetFactory.createHudLabel("Depth"), cc.xy(3, 5));
+		panel.add(depthField, cc.xy(3, 7));
+
 		return panel;
 	}
 
-	protected void setPositionX(float x) {
+	protected void setPosition(float x, float y, float z) {
 		setSelfChanging(true);
-		
+
 		Shape[] shapes = getSelectedShapes();
-		if (shapes.length > 0 && shapes[0].getX() != x) {
-			float y = shapes[0].getY();
-			float z = shapes[0].getZ();
+		if (shapes.length > 0 && (getX() != x || getY() != y || getZ() != z)) {
 			shapes[0].setPosition(x, y, z);
 		}
 
 		setSelfChanging(false);
 	}
 
-	protected void setPositionY(float y) {
+	protected void setSize(float width, float height, float depth) {
 		setSelfChanging(true);
 
 		Shape[] shapes = getSelectedShapes();
-		if (shapes.length > 0 && shapes[0].getY() != y) {
-			float x = shapes[0].getX();
-			float z = shapes[0].getZ();
-			shapes[0].setPosition(x, y, z);
+		if (shapes.length > 0
+				&& (getWidth() != width || getHeight() != height || getDepth() != depth)) {
+			shapes[0].setSize(width, height, depth);
 		}
 
 		setSelfChanging(false);
 	}
 
-	protected void setPositionZ(float z) {
-		setSelfChanging(true);
+	private float getX() {
+		return getSelectedShapes()[0].getX();
+	}
 
-		Shape[] shapes = getSelectedShapes();
-		if (shapes.length > 0 && shapes[0].getZ() != z) {
-			float x = shapes[0].getX();
-			float y = shapes[0].getY();
-			shapes[0].setPosition(x, y, z);
-		}
+	private float getY() {
+		return getSelectedShapes()[0].getY();
+	}
 
-		setSelfChanging(false);
+	private float getZ() {
+		return getSelectedShapes()[0].getZ();
+	}
+
+	private float getWidth() {
+		return getSelectedShapes()[0].getWidth();
+	}
+
+	private float getHeight() {
+		return getSelectedShapes()[0].getHeight();
+	}
+
+	private float getDepth() {
+		return getSelectedShapes()[0].getDepth();
 	}
 
 }
