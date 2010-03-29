e07f037e8107f800689a1c2c45548c3b6ff73ff7
Martin Lippert <lippert@acm.org>
2010-03-29 21:37:44 +0200

    exploding group childs now randomly rotated

--- desktop/com.sessionfive.app/src/com/sessionfive/core/ui/ExplodingLineGroupingLayouter.java
+++ desktop/com.sessionfive.app/src/com/sessionfive/core/ui/ExplodingLineGroupingLayouter.java
@@ -2,6 +2,7 @@ package com.sessionfive.core.ui;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 import com.sessionfive.core.Camera;
 import com.sessionfive.core.LayerType;
@@ -13,6 +14,7 @@ import com.sessionfive.core.ShapeRotation;
 public class ExplodingLineGroupingLayouter extends AbstractLayouter {
 
 	private static final String NAME = "Exploding Line Grouping";
+	private final Random random = new Random();
 
 	@Override
 	public String getName() {
@@ -59,7 +61,7 @@ public class ExplodingLineGroupingLayouter extends AbstractLayouter {
 
 				childY -= childSpace;
 				childZ -= 0.1f;
-				angleZ += 5;
+				angleZ = (random.nextInt() % 30);
 			}
 
 			if (childs.size() > 0) {
